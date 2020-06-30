9cd52cc9870e5f610068a91fb500ac544cd630f9
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-30 14:51:02 +0700

    added put request -> need to find again because if only do it once function will send back outdated ninja

--- routes/api.js
+++ routes/api.js
@@ -16,11 +16,15 @@ router.post('/ninjas', function(req, res,next){
 
 // update a ninja in the db
 router.put('/ninjas/:id', function(req, res,next){
-    res.send({type: 'PUT'});
+    Ninja.findByIdAndUpdate({_id:req.params.id},req.body).then(function(){
+        Ninja.findOne({_id:req.params.id}).then(function(ninja){
+            res.send(ninja)
+        })
+    })
 });
 
 // delete a ninja from the db
-router.delete('/ninjas/:id', function(req, res){
+router.delete('/ninjas/:id', function(req, res,next){
     Ninja.findByIdAndRemove({_id:req.params.id}).then(function(ninja) {
         res.send(ninja)
     })
