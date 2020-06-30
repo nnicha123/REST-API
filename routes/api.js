2632725573eff08f4387c5bb83a46386799f36d8
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-30 14:44:52 +0700

    delete requests

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,9 @@ router.put('/ninjas/:id', function(req, res,next){
 
 // delete a ninja from the db
 router.delete('/ninjas/:id', function(req, res){
-    res.send({type: 'DELETE'});
+    Ninja.findByIdAndRemove({_id:req.params.id}).then(function(ninja) {
+        res.send(ninja)
+    })
 });
 
 module.exports = router;
